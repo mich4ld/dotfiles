cc3771e4d78762d24edea0b95c5c0b020e70b3e6
Michał <43048524+mich4ld@users.noreply.github.com>
2022-02-15 20:04:45 +0100

    Update settings.lua

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,12 +1,21 @@
 local lspconfig = require('lspconfig')
 
-local servers = { 'rust_analyzer', 'tsserver' }
+local servers = { 
+  'rust_analyzer', 
+  'tsserver',
+  'bashls',
+  'emmet_ls'
+}
+
+require('completion')
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 for _, lsp in pairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
     flags = {
-      -- This will be the default in neovim 0.7+
       debounce_text_changes = 150,
+      capabilities = capabilities
     }
   }
 end
@@ -37,4 +46,3 @@ require("indent_blankline").setup {
 
 
 require('feline').setup({ components = require('catppuccin.core.integrations.feline') })
-
