3800d2e538db451af4ecc407f23fa8e9e2fd267f
Michał <43048524+mich4ld@users.noreply.github.com>
2022-02-18 15:08:37 +0100

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,29 +7,50 @@ return require('packer').startup(function()
     use 'neovim/nvim-lspconfig'
     use 'sbdchd/neoformat'
     use 'simrat39/rust-tools.nvim'
-		
+
     -- Completion:
+    use 'hrsh7th/vim-vsnip'
+    use 'hrsh7th/vim-vsnip-integ'
     use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-nvim-lsp'
     
     use {
-    "kyazdani42/nvim-tree.lua",
-    requires = "kyazdani42/nvim-web-devicons" ,
-    wants = "nvim-web-devicons",
-    config = function()
-      require("nvim-web-devicons").setup()
-
-      require("nvim-tree").setup {
-        hijack_cursor = true,
-        auto_close = true,
-        view = {
-          width = 40
+      "kyazdani42/nvim-tree.lua",
+      requires = "kyazdani42/nvim-web-devicons" ,
+      wants = "nvim-web-devicons",
+      config = function()
+        require("nvim-web-devicons").setup()
+        require("nvim-tree").setup {
+          hijack_cursor = true,
+          auto_close = true,
+          view = {
+            width = 40
+          }
         }
-            }
-        end
+      end
     }
+
+    use 'akinsho/bufferline.nvim'
+    use {
+      'lewis6991/gitsigns.nvim',
+      requires = {
+        'nvim-lua/plenary.nvim'
+      },
+      config = function()
+        require('gitsigns').setup({
+            attach_to_untracked = true,
+            current_line_blame_opts = {
+              virt_text = true,
+              virt_text_pos = 'overlay', -- 'eol' | 'overlay' | 'right_align'
+              delay = 1000,
+              ignore_whitespace = false,
+            },
+          })
+      end
+    }
+
 	use({
        "catppuccin/nvim",
            as = "catppuccin"
